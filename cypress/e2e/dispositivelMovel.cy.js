9e5f22e1f6f4858cac67cc7359fc33ef14511dd6
CleytonLu <cleyton.cabral>
2024-05-03 10:42:41 -0300

    Teste com viewport de mobile

---
+++ cypress/e2e/dispositivelMovel.cy.js
@@ -0,0 +1,19 @@
+describe("Testing mobile", () => {
+    it("It should exists button menu burguer", () => {
+        cy.visit("/");
+
+        cy.viewport(375, 677)
+
+        cy.getByData("botao-login").click();
+        cy.getByData("email-input").type("cleyton@gmail.com");
+        cy.getByData("senha-input").type("123456");
+        cy.getByData("botao-enviar").click();
+
+        cy.location("pathname").should("eq", "/home");
+
+        cy.getByData("menu-burguer").click();
+        cy.getByData("menu-lateral").find("a").eq(3).click();
+
+        cy.location("pathname").eq("/home/investimentos");
+    })
+});
